27fe556ec75c6fafa679f0191a4c4c75b6b6ea2b
Dingalibala99 <makadunyiswed@gmail.com>
2022-04-26 11:10:01 +0200

    fifth commit

---
+++ task0.5.js
@@ -0,0 +1,7 @@
+function triangleArea(length1,length2,length3) {
+    // length2 is a breath and length3 is the height
+    let bh = length2 * length3;
+    let area = bh / 2;
+    console.log(area +" is the area of the triangle");
+}
+triangleArea(2,4,5);
