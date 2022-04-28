bca7e2607619f59889831a082449bda5ad6cddd7
Dingalibala99 <102686868+Dingalibala99@users.noreply.github.com>
2022-04-28 12:22:36 +0200

    Update task0.10.js

--- task0.10.js
+++ task0.10.js
@@ -1,15 +1,16 @@
-function commonLetters(firstNoun, secNoun) {
-    let stringOne = firstNoun.toLowerCase();
-    let stringTwo = secNoun.toLowerCase();
-  let duplicateLetter = "";
-  for (let i = 0; i < stringOne.length; i += 1) {
-    if (duplicateLetter.indexOf(stringOne[i]) === -1) {
-      if (stringTwo.indexOf(stringOne[i]) !== -1) {
-        duplicateLetter += stringOne[i];
-      }
+function commonLetters(firstNoun, secondNoun) {
+  let stringOne = firstNoun.toLowerCase();
+  let stringTwo = secondNoun.toLowerCase();
+  let output = "common letter: ";
+let duplicateLetter = "";
+for (let i = 0; i < stringOne.length; i += 1) {
+  if (duplicateLetter.indexOf(stringOne[i]) === -1) {
+    if (stringTwo.indexOf(stringOne[i]) !== -1) {
+      duplicateLetter += stringOne[i];
     }
   }
-  return [...duplicateLetter];
+}
+return output + [...duplicateLetter];
 };
 
-console.log(commonLetters("Oasis","Orbit"));
+console.log(commonLetters("Andile","Anele"));
