470232cbc5bf1389c205466a974a4cd4e24e079e
Itumeleng1962 <77567573+Itumeleng1962@users.noreply.github.com>
2022-04-29 10:40:05 +0200

    Update task0.6.js

--- task0.6.js
+++ task0.6.js
@@ -1,20 +1,11 @@
-function getMaximum(value1, value2, value3) {
-    if (value1[0] !== undefined || value2[0] !== undefined || value3[0] !== undefined || (otherValues.length !== 0 && otherValues[0][0] !== undefined)) {
-        return "function expecting a lot of numbers, not arrays or lists.";
-    }
-
-    let maxnumNumber = "";
-    if (value1 > value2 && value1 > value3) {
-        maxnumNumber = value1;
-    } else if (number2 > value3) {
-      maxnumNumber = value2;
-    } else {
-        maxnumNumber = value3;
-    }
-
-    for (let i = 0; i < otherValues.length; i++) {
-        if (maxnumNumber < otherValues[i]) {
-            maxnumNumber = otherValues[i];
-        }
-    }
-    return maxnumNumber;
+function getMaximum(val1, val2, val3) {
+  if (val1 > val2 && val2 > val2) {
+    maximumValue = val1;
+  } else if (val2 > val2) {
+    maximumValue = val2;
+  } else {
+    maximumValue = val3;
+    return maximumValue;
+  }
+}
+console.log(getMaximum(5,10,15));
